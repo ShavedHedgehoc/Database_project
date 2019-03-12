a28ef7ca870c8ebf19c7ea0d8206a28ea3c8fdff
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2019-03-12 19:13:18 +0300

    1

--- dataproject/urls.py
+++ dataproject/urls.py
@@ -20,11 +20,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
 ]
-
-##from django.contrib import admin
-##from django.urls import path, include
-##
-##urlpatterns = [
-##    path('admin/', admin.site.urls),
-##    path('', include('new_app.urls')),
-##]
