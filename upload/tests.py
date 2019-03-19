d2d0adf524ae51be1bf1e33ae5cb5b0e03f20a74
yojh <eagle221174@gmail.com>
2019-03-19 22:42:30 +0300

    1

--- upload/tests.py
+++ upload/tests.py
@@ -1,25 +1,18 @@
 from django.test import TestCase
-from .views import read_xl_file
+
+from upload.views import read_xl_file
+from upload.views import get_date
+
 import openpyxl
 import tempfile
 import shutil
 import pandas as pd
 import os
 
-
 # Create your tests here.
 
 
-from unittest import TestCase
-
-
 class read_xl_file_tests(TestCase):
-
-    def ffff(self):
-        self.fname = 'not_existing_file.xlsx'
-        self.assertRaises(FileNotFoundError, read_xl_file, self.fname)
-            
-
     def test_empty_xls_xlsx_file(self):
         files = []
         wb = openpyxl.Workbook()
@@ -48,3 +41,14 @@ class read_xl_file_tests(TestCase):
         self.assertTrue(test_df[1] == "Файл не является файлом .xls .xlsx")
         self.assertIsNone(test_df[0])
         os.remove('not_excel_file.txt')
+
+
+class test_get_date_function(TestCase):
+    def common_date_test(self):
+        pattern = "2019-11-01 23:15:50"
+        test_date = get_date("2019-11-01 23:15:50")
+        self.assertTrue(test_date == "01-11-2019")
+
+
+if __name__ == '__main__':
+    unittest.main()
